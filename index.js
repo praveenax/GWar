00ada3b3c640382b4fc328d1a9888ede5540119a
Praveen <xav.axp@gmail.com>
2017-11-03 09:41:50 +0530

    added fbase admin

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var firebase = require("firebase-admin");
+
+var serviceAccount = require("./key/serviceAccountKey.json");
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://gjden-ac410.firebaseio.com"
+});
+
+
 var http = require('http').Server(app);
 
 
@@ -19,4 +29,10 @@ var server = http.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
 
     console.log('App listening at http://%s:%s', host, port);
+});
+
+
+
+firebase.database().ref('/list').on('value', function(postSnapshot) {
+   console.log(postSnapshot.val()); 
 });
